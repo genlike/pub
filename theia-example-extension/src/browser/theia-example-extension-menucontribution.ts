d19247982d00e3eaebc343e7d2ab819858db97a7
GenLike <ricardo.f.oliveira.da.silva@tecnico.ulisboa.pt>
2022-11-09 03:45:29 +0000

    readonly mode remove commands

--- theia-example-extension/src/browser/theia-example-extension-menucontribution.ts
+++ theia-example-extension/src/browser/theia-example-extension-menucontribution.ts
@@ -2,6 +2,7 @@ import { injectable } from '@theia/core/shared/inversify';
 import { CommandContribution, CommandRegistry, MenuContribution, MenuModelRegistry } from '@theia/core/lib/common';
 import { WorkspaceCommands } from '@theia/workspace/lib/browser';
 import { TerminalMenus } from '@theia/terminal/lib/browser/terminal-frontend-contribution';
+import { TaskCommands } from '@theia/task/lib/browser/task-frontend-contribution';
 import { CommonMenus } from '@theia/core/lib/browser';
 import { TerminalWidgetImpl } from '@theia/terminal/lib/browser/terminal-widget-impl';
 
@@ -22,7 +23,7 @@ export class TheiaExampleMenuContribution implements MenuContribution, CommandCo
         menus.unregisterMenuAction(TerminalMenus.TERMINAL_TASKS_CONFIG[TerminalMenus.TERMINAL_TASKS_CONFIG.length - 1], TerminalMenus.TERMINAL);
         menus.unregisterMenuAction(TerminalMenus.TERMINAL_NEW[TerminalMenus.TERMINAL_NEW.length - 1], TerminalMenus.TERMINAL);
 
-        menus.unregisterMenuNode('7_terminal');
+        //menus.unregisterMenuNode(TerminalMenus.TERMINAL[TerminalMenus.TERMINAL.length-1]);
 
         //menus.unregisterMenuAction(CommonMenus.[CommonMenus.FILE_SETTINGS.length - 1], CommonMenus.FILE);
         //menus.unregisterMenuAction(CommonMenus.FILE_NEW[CommonMenus.FILE_NEW.length - 1], CommonMenus.FILE);
@@ -35,6 +36,15 @@ export class TheiaExampleMenuContribution implements MenuContribution, CommandCo
         commands.unregisterCommand(WorkspaceCommands.NEW_FOLDER);
         commands.unregisterCommand(WorkspaceCommands.FILE_DUPLICATE);
         commands.unregisterCommand(WorkspaceCommands.FILE_RENAME);
+
+
+        commands.unregisterCommand(TaskCommands.TASK_RUN);
+        commands.unregisterCommand(TaskCommands.TASK_RUN_BUILD);
+        commands.unregisterCommand(TaskCommands.TASK_RUN_LAST);
+        commands.unregisterCommand(TaskCommands.TASK_RUN_TEST);
+        commands.unregisterCommand(TaskCommands.TASK_RUN_TEXT);
+        commands.unregisterCommand(TaskCommands.TASK_CONFIGURE);
+        commands.unregisterCommand(TaskCommands.TASK_SHOW_RUNNING);
     }
 }
 
